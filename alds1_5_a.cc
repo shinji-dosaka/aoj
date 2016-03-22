519b6e91a34fccfb70b950f725bec45f5d769c15
Dosaka,Shinji <shinji-dosaka@users.noreply.github.com>
2016-03-22 23:15:32 +0900

    add ALDS1_5-A

---
+++ alds1_5_a.cc
@@ -0,0 +1,36 @@
+// -*- mode:c++; coding:utf-8; c-basic-offset:2; -*-
+// ALDS1_5-A: Exhaustive Search
+
+#include <cstdio>
+
+static bool solve(const int m, const int as[], const unsigned int n)
+{
+  if (m == 0) {
+    return true;
+  } else if (m < 0 || n == 0) {
+    return false;
+  } else {
+    return solve(m-as[0], as+1, n-1) || solve(m, as+1, n-1);
+  }
+}
+
+int main()
+{
+  unsigned int n;
+  std::scanf("%d", &n);
+  int as[n];
+  for (auto i = 0u; i < n; ++i) {
+    std::scanf("%d", &as[i]);
+  }
+
+  unsigned int q;
+  std::scanf("%d", &q);
+  for (auto i = 0u; i < q; ++i) {
+    int m;
+    std::scanf("%d", &m);
+    auto is_solved = solve(m, as, n);
+    std::printf("%s\n", is_solved ? "yes" : "no");
+  }
+}
+
+// eof
