6a07abe7188c728344c7c94a4d704e23716d2820
Dosaka,Shinji <shinji-dosaka@users.noreply.github.com>
2016-04-08 18:47:46 +0900

    change struct Node to inner struct, etc

--- alds1_7_a.cc
+++ alds1_7_a.cc
@@ -3,26 +3,18 @@
 
 #include <cstdio>
 
-const int NIL = -1;
-
-struct Node {
-  // id is index of nodes
-  int parent = NIL;
-  int left_child = NIL;
-  int right_sibling = NIL;
-  unsigned int depth = 0u;
-
-  const char* type() const
-  {
-    auto type =
-      parent == NIL ? "root" :
-      left_child == NIL ? "leaf" :
-      "internal node";
-    return type;
-  }    
-};
-
 class RootedTree {
+  static const int NIL = -1;
+
+  struct Node {
+    // id is index of nodes_
+    int parent = NIL;
+    int sibling = NIL;
+    int first_child = NIL;
+    unsigned int depth = 0u;
+  };
+
+private:
   unsigned int n_;
   Node* nodes_;
 
@@ -40,47 +32,58 @@ public:
 
   void print_nodes() const
   {
-    for (auto i = 0u; i < n_; ++i) {
-      print_node(i);
+    for (auto id = 0u; id < n_; ++id) {
+      print_node(id);
     }
   }
 
 private:
+  void set_depth_internal(int id, unsigned int depth)
+  {
+    if (id == NIL) {
+      return;
+    }
+    auto& node = nodes_[id];
+    node.depth = depth;
+    set_depth_internal(node.sibling, depth);
+    set_depth_internal(node.first_child, depth + 1);
+  }
+
   int root() const
   {
-    for (auto i = 0u; i < n_; ++i) {
-      if (nodes_[i].parent == NIL) {
-        return i;
+    for (auto id = 0u; id < n_; ++id) {
+      const auto& node = nodes_[id];
+      if (node.parent == NIL) {
+        return id;
       }
     }
     return NIL;
   }
 
-  void set_depth_internal(unsigned int i, unsigned int depth)
+  void print_node(unsigned int id) const
   {
-    nodes_[i].depth = depth;
-    if (nodes_[i].right_sibling != NIL) {
-      set_depth_internal(nodes_[i].right_sibling, depth);
-    }
-    if (nodes_[i].left_child != NIL) {
-      set_depth_internal(nodes_[i].left_child, depth+1);
-    }
-  }
-
-  void print_node(unsigned int i) const
-  {
-    auto node = nodes_[i];
-    std::printf("node %u: parent = %d, depth = %u, ", i, node.parent, node.depth);
-    std::printf("%s, [", node.type());
-    int next_c = node.left_child;
+    const auto& node = nodes_[id];
+    std::printf("node %u: parent = %d, depth = %u, ", id, node.parent, node.depth);
+    std::printf("%s, [", type(id));
+    int next_c = node.first_child;
     auto sep = "";
     while (next_c != NIL) {
       std::printf("%s%d", sep, next_c);
       sep = ", ";
-      next_c = nodes_[next_c].right_sibling;
+      next_c = nodes_[next_c].sibling;
     }
     std::printf("]\n");
   }
+
+  const char* type(unsigned int id) const
+  {
+    const auto& node = nodes_[id];
+    auto type =
+      node.parent == NIL ? "root" :
+      node.first_child == NIL ? "leaf" :
+      "internal node";
+    return type;
+  }    
 };
 
 int main()
@@ -93,16 +96,16 @@ int main()
     unsigned int id, k, c;
     std::scanf("%u %u", &id, &k);
 
-    int* next_p = &tree[id].left_child;
+    int* next_p = &tree[id].first_child;
     for (auto j = 0u; j < k; ++j) {
       std::scanf("%u", &c);
       tree[c].parent = id;
       *next_p = c;
-      next_p = &tree[c].right_sibling;
+      next_p = &tree[c].sibling;
     }
   }
-
   tree.set_depth();
+
   tree.print_nodes();
 }
 
