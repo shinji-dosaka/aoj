c2d61e4db7a7761159c34a24c7cb8095dfbed15b
Dosaka,Shinji <shinji-dosaka@users.noreply.github.com>
2016-04-27 18:36:55 +0900

    cast

--- alds1_10_c.cc
+++ alds1_10_c.cc
@@ -9,19 +9,19 @@ static unsigned int lcs(const char* x, const char* y)
 {
   auto m = static_cast<int>(std::strlen(x));
   auto n = static_cast<int>(std::strlen(y));
-  unsigned int c[m+1][n+1]; // extend -1
+  unsigned int c1[m+1][n+1]; // extend for -1
+  auto c = reinterpret_cast<int(*)[n+1]>(&c1[1][1]); // tricky?
 
-  for (auto i = 0; i < m+1; ++i) c[i][0] = 0u;
-  for (auto j = 1; j < n+1; ++j) c[0][j] = 0u;
-
+  for (auto i = -1; i < m; ++i) c[i][-1] = 0u;
+  for (auto j = 0; j < n; ++j) c[-1][j] = 0u;
   for (auto i = 0; i < m; ++i) {
     for (auto j = 0; j < n; ++j) {
-      c[i+1][j+1] = (x[i] == y[j])
-        ? c[i][j] + 1
-        : std::max(c[i+1][j], c[i][j+1]);
+      c[i][j] = (x[i] == y[j])
+        ? c[i-1][j-1] + 1
+        : std::max(c[i][j-1], c[i-1][j]);
     }
   }
-  return c[m][n];
+  return c[m-1][n-1];
 }
 
 int main()
